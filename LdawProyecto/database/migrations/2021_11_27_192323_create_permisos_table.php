77abdd7fb647e4774e39a4ce16ff62a742b79948
feeramezquita <a01421954@itesm.mx>
2021-11-27 13:57:54 -0600

    Migrations created

--- LdawProyecto/database/migrations/2021_11_27_192323_create_permisos_table.php
+++ LdawProyecto/database/migrations/2021_11_27_192323_create_permisos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreatePermisosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('id_user');
-            $table->string('name'); 
-            $table->string('password');
-            $table->rememberToken();
-            $table->timestamps();
+        Schema::create('permisos', function (Blueprint $table) {
+            $table->id('id_permiso');
+            $table->string('nombre_permiso');
         });
     }
 
@@ -29,6 +26,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('permisos');
     }
 }
--- LdawProyecto/database/migrations/2021_11_27_195108_create_rolehaspermisos_table.php
+++ LdawProyecto/database/migrations/2021_11_27_195108_create_rolehaspermisos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateRolehaspermisosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,9 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id('id_user');
-            $table->string('name'); 
-            $table->string('password');
-            $table->rememberToken();
+        Schema::create('rolehaspermisos', function (Blueprint $table) {
+            $table->id('id_rol');
+            $table->foreignID('id_permiso');
             $table->timestamps();
         });
     }
@@ -29,6 +27,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('rolehaspermisos');
     }
 }
