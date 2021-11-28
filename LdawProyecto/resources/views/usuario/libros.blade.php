46b8f9542183b37d1e95acebb8190d5740a88634
a01173530 <A01173530@itesm.mx>
2021-11-27 19:48:47 -0600

    Forms usuario

--- LdawProyecto/resources/views/usuario/libros.blade.php
+++ LdawProyecto/resources/views/usuario/libros.blade.php
@@ -1,1 +1,22 @@
 <!-- Mostar lista de libros Consulta-->
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">Nombre de Usuario</th>
+      <th scope="col">Comentario</th>
+      <th scope="col">Calificación</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Mark</td>
+      <td>cool</td>
+      <td>5</td>
+    </tr>
+    <tr>
+      <td>Jacob</td>
+      <td>estuvo X</td>
+      <td>4</td>
+    </tr>
+  </tbody>
+</table>
